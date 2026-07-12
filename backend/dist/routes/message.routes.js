32356599766c55ee38948e1afe966035f0530bd1
Erfan <erfan8104taheri@gamil.com>
2026-07-12 09:18:52 +0330

    chore: remove build artifacts and uploads from tracking

--- backend/dist/routes/message.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const message_controller_1 = require("../controllers/message.controller");
-const auth_middleware_1 = require("../middleware/auth.middleware");
-const router = (0, express_1.Router)();
-// دریافت پیام‌های چت بر اساس آی‌دی قرارداد
-router.get("/history/:contractId", auth_middleware_1.authMiddleware, message_controller_1.getChatHistory);
-exports.default = router;
--- backend/dist/routes/profile.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const profile_controller_1 = require("../controllers/profile.controller");
-const auth_middleware_1 = require("../middleware/auth.middleware");
-const router = (0, express_1.Router)();
-router.put("/employer", auth_middleware_1.authMiddleware, profile_controller_1.updateEmployerProfile);
-router.put("/freelancer", auth_middleware_1.authMiddleware, profile_controller_1.updateFreelancerProfile);
-exports.default = router;
